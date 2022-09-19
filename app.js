c0f673e322458de429a71951be9cf7cff40d725c
Nathan Lobo <113709797+nathanlobo@users.noreply.github.com>
2022-09-19 11:46:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ menu.addEventListener('click', function(){
 var icon = document.getElementById("icon");
 icon.onclick = function(){
     document.body.classList.toggle("dark-theme");
-    if(document.body.classList.contains("dark-theme")){
-        icon.src = "images/sun.png";
+    if(document.body.classList.contains("light-theme")){
+        icon.src = "images/moon.png";
     }
     else{
-        icon.src = "images/moon.png"
+        icon.src = "images/sun.png"
     }
 }
