04347a2ae67de61c1fca449a8c30fa7c77504e10
Nathan Lobo <113709797+nathanlobo@users.noreply.github.com>
2022-09-19 09:06:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var icon = document.getElementById("icon");
 icon.onclick = function(){
     document.body.classList.toggle("dark-theme");
     if(document.body.classList.contains("dark-theme")){
-        icon.src = "images/Sun.png";
+        icon.src = "images/sun.png";
     }
     else{
         icon.src = "images/moon.png"
